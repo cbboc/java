94d807c7ed6f6293a88dc6885defc5e20c5a5639
JerrySwan <JerrySwan@users.noreply.github.com>
2016-04-08 10:31:21 +0100

    Update TestProblemInstance.java

--- src/cbboc/test/TestProblemInstance.java
+++ src/cbboc/test/TestProblemInstance.java
@@ -15,13 +15,21 @@ import cbboc.ProblemInstance;
 public class TestProblemInstance {
 
 	@Test
-	public void test() throws FileNotFoundException, IOException {
-		
+	public void test1() throws FileNotFoundException, IOException {
 		String root = System.getProperty( "user.dir" );
-		String path = root + "/resources/test/toy/testing/toy.txt";
+		String path = root + "/resources/toy1.txt";
 		ProblemInstance instance = new ProblemInstance( new FileInputStream( new File( path ) ) );
 		assertEquals( 8, instance.getNumGenes() );
-		assertEquals( 100000, instance.getMaxEvalsPerInstance() );
+		// assertEquals( 100000, instance.getMaxEvalsPerInstance() );
+	}
+
+	@Test
+	public void test2() throws FileNotFoundException, IOException {
+		String root = System.getProperty( "user.dir" );
+		String path = root + "/resources/toy2.txt";
+		ProblemInstance instance = new ProblemInstance( new FileInputStream( new File( path ) ) );
+		assertEquals( 13, instance.getNumGenes() );
+		// assertEquals( 100000, instance.getMaxEvalsPerInstance() );
 	}
 }
 
