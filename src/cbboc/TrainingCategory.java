c03c7c092650e9b4dc11646dceb8b0abaa43ff18
JerrySwan <JerrySwan@users.noreply.github.com>
2016-04-14 19:49:49 +0100

    Update TrainingCategory.java
    Added getNumericCode.

--- src/cbboc/TrainingCategory.java
+++ src/cbboc/TrainingCategory.java
@@ -2,19 +2,19 @@ package cbboc;
 
 public enum TrainingCategory {
 
-	NONE( 0 ), SHORT( 1 ), LONG( 10 );
+	NONE( 0, 1 ), SHORT( 1, 1 ), LONG( 10, 2 );
 
 	///////////////////////////////
 	
-	private final int multiplier;
+	private final int multiplier, numericCode;
 	
-	private TrainingCategory( int multiplier ) {
+	private TrainingCategory( int multiplier, int numericCode ) {
 		this.multiplier = multiplier;
+		this.numericCode = numericCode;
 	}
 	
-	public final long getMultiplier() {
-		return multiplier;
-	}
+	public final long getMultiplier() { return multiplier; }
+	public final int getNumericCode() { return numericCode; }	
 }
 
 // End ///////////////////////////////////////////////////////////////
